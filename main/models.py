de6b5285531950b989d0abdbf62a6ed6d6bb0030
Shelby010 <72750844+Shelby010@users.noreply.github.com>
2021-01-30 23:17:13 -0800

    Update models.py

--- main/models.py
+++ main/models.py
@@ -5,12 +5,12 @@ from django.db.models import Q
 
 User = settings.AUTH_USER_MODEL
 
-class TweetLike(models.Model):
+class TwitterModel(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     tweet = models.ForeignKey("Tweet", on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True)
 
-class TweetQuerySet(models.QuerySet):
+class TwitterQuerySet(models.QuerySet):
     def by_username(self, username):
         return self.filter(user__username__iexact=username)
 
@@ -24,21 +24,20 @@ class TweetQuerySet(models.QuerySet):
             Q(user=user)
         ).distinct().order_by("-timestamp")
 
-class TweetManager(models.Manager):
+class TwitterManager(models.Manager):
     def get_queryset(self, *args, **kwargs):
         return TweetQuerySet(self.model, using=self._db)
 
     def feed(self, user):
         return self.get_queryset().feed(user)
 
-class Tweet(models.Model):
+class Twitter(models.Model):
     parent = models.ForeignKey("self", null=True, on_delete=models.SET_NULL)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tweets") 
-    likes = models.ManyToManyField(User, related_name='tweet_user', blank=True, through=TweetLike)
+    likes = models.ManyToManyField(User, related_name='twitter_user', blank=True, through=TwitteModel)
     content = models.TextField(blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     objects = TweetManager()
-
     
     class Meta:
         ordering = ['-id']
